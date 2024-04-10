470db7d02c83b87ada0525dc32912c8b23a78ffb
codebyayush <ayushpal6939@gmail.com>
2024-04-10 18:13:27 +0530

    email verification added

--- src/components/Pages/ProfileForm.jsx
+++ src/components/Pages/ProfileForm.jsx
@@ -4,83 +4,118 @@ import { IoGlobeSharp } from "react-icons/io5";
 import ItemContext from "../../Store/ItemContext";
 
 const ProfileForm = (props) => {
-    const ctx = useContext(ItemContext)
+  const ctx = useContext(ItemContext);
 
   const nameRef = useRef();
   const urlRef = useRef();
 
   useEffect(() => {
     const fetchData = async () => {
-      
-      const token = localStorage.getItem("token")
-      
-      const resp = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCJO3nf1rN9288u3VAFDm0kC3eqhRSqPKc", {
-        method: "POST",
-        body: JSON.stringify({
-          idToken: token,
-        }),
-        headers: {"Content-Type": "application/json"}
-    })
+      const token = localStorage.getItem("token");
 
-      if(resp.ok){
+      const resp = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyCJO3nf1rN9288u3VAFDm0kC3eqhRSqPKc",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: token,
+          }),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+
+      if (resp.ok) {
         const data = await resp.json();
         console.log("fetched data", data);
 
         const userArr = data.users;
 
         userArr.map((item) => {
-            nameRef.current.value = item.displayName;
-            urlRef.current.value = item.photoUrl;
-        })
+          nameRef.current.value = item.displayName;
+          urlRef.current.value = item.photoUrl;
+        });
 
         props.handleProfile(true);
-
-      }else{
+      } else {
         const data = await resp.json();
         console.error("ERROR FETCHING DATA", data);
       }
-  }
+    };
     fetchData();
-}, [])
+  }, []);
 
+  const verifyEmailHandler = async () => {
+    const token = localStorage.getItem("token");
+
+    
+    try {
+      const resp = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyCJO3nf1rN9288u3VAFDm0kC3eqhRSqPKc",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            requestType: "VERIFY_EMAIL",
+            idToken: token,
+          }),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      
+      if (resp.ok) {
+        const data = await resp.json();
+        console.log("VERIFICATION MAIL SENT TO: ", data.email);
+        alert("VERIFICATION MAIL SENT");
+      } else {
+        const data = await resp.json();
+        console.log("FAILED TO SENT CODE", data.error.message);
+      }
+    } catch (error) {
+        console.error("ERROR SENDING MAIL:", error)
+    }
+
+  };
 
   const submitHandler = async (e) => {
-        e.preventDefault();
+    e.preventDefault();
 
-        const enteredName = nameRef.current.value;
-        const enteredUrl = urlRef.current.value;
+    const enteredName = nameRef.current.value;
+    const enteredUrl = urlRef.current.value;
 
-        const token = localStorage.getItem("token")
+    const token = localStorage.getItem("token");
 
-        const resp = await fetch("https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCJO3nf1rN9288u3VAFDm0kC3eqhRSqPKc", {
-            method: "POST",
-            body: JSON.stringify({
-                idToken: token,
-                displayName: enteredName,
-                photoUrl: enteredUrl,
-                returnSecureToken: true,
-            }),
-            headers: {"Content-Type": "application/json"}
-        })
+    const resp = await fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyCJO3nf1rN9288u3VAFDm0kC3eqhRSqPKc",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          idToken: token,
+          displayName: enteredName,
+          photoUrl: enteredUrl,
+          returnSecureToken: true,
+        }),
+        headers: { "Content-Type": "application/json" },
+      }
+    );
 
-        if(resp.ok){
-            const data = await resp.json();
-            console.log("profile updated: ", data)
-        }
-        else{
-            const data = await resp.json();
-            console.log("ERROR: ", data)
-        }
-  }
-
+    if (resp.ok) {
+      const data = await resp.json();
+      console.log("profile updated: ", data);
+    } else {
+      const data = await resp.json();
+      console.log("ERROR: ", data);
+    }
+  };
 
   return (
     <>
       <div className=" flex justify-end">
-        <div className=" mt-10 w-2/3 mr-2">
+        <div className=" mt-10 w-2/3 mr-4">
           <div className="m-2 flex justify-between text-xl font-medium">
             <h1>Contact Details</h1>
-            <button className=" border border-red-300 rounded-md p-1 font-medium " onClick={ctx.logoutHandler}>
+            <button
+              className=" border border-red-300 rounded-md p-1 font-medium "
+              onClick={ctx.logoutHandler}
+            >
               Cancel
             </button>
           </div>
@@ -121,12 +156,22 @@ const ProfileForm = (props) => {
                   />
                 </div>
               </div>
-              <button type="submit" className="mt-5 m-1 bg-red-500 text-white border border-red-400 rounded-md p-1 font-medium ">
+              <button
+                type="submit"
+                className="mt-5 m-1 bg-red-500 text-white border border-red-400 rounded-md p-1 font-medium "
+              >
                 Update
+              </button>
+              <button
+                onClick={verifyEmailHandler}
+                className="italic me-4 border border-red-400 ms-4 rounded-md p-1 bg-red-400 font-medium text-white"
+              >
+                Verify Email
               </button>
             </form>
           </div>
-          <hr className="border border-gray-400"/>
+
+          <hr className="border border-gray-400" />
         </div>
       </div>
     </>
